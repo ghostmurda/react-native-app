eaa2fded8a8182a58efea8436a3da50591868915
Daniel Simonov <sk8kmd@gmail.com>
2020-11-18 15:48:40 +0300

    Update home page markup, styles, content

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Body, Left, ListItem, Text, Thumbnail, List} from "native-base";
+import {Body, Left, ListItem, Text, Thumbnail, List, Icon, Separator, Right, Button} from "native-base";
 import {selectorUserInfo} from "../store/auth/selectors";
 import {connect} from "react-redux";
+import mainColor from "../styles";
 
 const mapStateToProps = (state) => (
     {
@@ -12,17 +13,74 @@ const mapStateToProps = (state) => (
 function HomePage(props){
     return (
         <List>
+            <Separator bordered>
+                <Text style={{fontSize: 16}}>My profile</Text>
+            </Separator>
             <ListItem avatar>
                 <Left>
                     <Thumbnail source={{uri: props.userInfo.photoUrl}} />
                 </Left>
                 <Body>
                     <Text>{props.userInfo.name}</Text>
-                    <Text note>Id: @testid</Text>
+                    <Text note>Test status</Text>
                 </Body>
             </ListItem>
-            <ListItem >
-                <Text>Email: {props.userInfo.email}</Text>
+            <ListItem>
+                <Icon name="mail" style={{color: mainColor}} />
+                <Text style={{marginLeft: 16}}>{props.userInfo.email}</Text>
+            </ListItem>
+            <ListItem last>
+                <Icon name="md-at" style={{color: mainColor}} />
+                <Text style={{marginLeft: 16}}>testid</Text>
+            </ListItem>
+
+            <Separator bordered>
+                <Text style={{fontSize: 16}}>My apps</Text>
+            </Separator>
+            <ListItem thumbnail>
+                <Left>
+                    <Thumbnail square style={{borderRadius: 10}} source={{ uri: props.userInfo.photoUrl }} />
+                </Left>
+                <Body>
+                    <Text>Hardcode app name</Text>
+                    <Text note>Game</Text>
+
+                </Body>
+                <Right>
+                    <Button transparent>
+                        <Text>View</Text>
+                    </Button>
+                </Right>
+            </ListItem>
+            <ListItem thumbnail>
+                <Left>
+                    <Thumbnail square style={{borderRadius: 10}} source={{ uri: props.userInfo.photoUrl }} />
+                </Left>
+                <Body>
+                    <Text>Hardcode app name</Text>
+                    <Text note>Game</Text>
+
+                </Body>
+                <Right>
+                    <Button transparent>
+                        <Text>View</Text>
+                    </Button>
+                </Right>
+            </ListItem>
+            <ListItem thumbnail>
+                <Left>
+                    <Thumbnail square style={{borderRadius: 10}} source={{ uri: props.userInfo.photoUrl }} />
+                </Left>
+                <Body>
+                    <Text>Hardcode app name</Text>
+                    <Text note>Game</Text>
+
+                </Body>
+                <Right>
+                    <Button transparent>
+                        <Text>View</Text>
+                    </Button>
+                </Right>
             </ListItem>
         </List>
     );
